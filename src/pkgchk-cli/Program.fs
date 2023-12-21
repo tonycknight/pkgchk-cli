58068ce58638de170b7ca24c56e3c8805664e838
tonycknight <tonycknight@users.noreply.github.com>
2023-12-21 14:20:02 +0000

    first cut

---
+++ src/pkgchk-cli/Program.fs
@@ -0,0 +1,32 @@
+﻿namespace pkgchk
+
+open System
+
+module Program =
+    [<EntryPoint>]
+    let main args =
+
+        let path =
+            match args with
+            | [||] -> ""
+            | [| p |] -> p
+            | [| p; _ |] -> p
+
+        let r = Sca.createProcess path true |> Sca.get
+
+        match r with
+        | Choice1Of2 json ->
+            match Sca.parse json with
+            | Choice1Of2 [] ->
+                Console.Out.WriteLine("No vulnerabilities found!")
+                0
+            | Choice1Of2 hits ->
+                Console.Out.WriteLine("Vulnerabilities found!")
+                hits |> Sca.formatHits |> Console.Out.WriteLine
+                1
+            | Choice2Of2 error ->
+                Console.Error.WriteLine error
+                99
+        | Choice2Of2 err ->
+            Console.Error.WriteLine err
+            99
---
+++ src/pkgchk-cli/Sca.fs
@@ -0,0 +1,115 @@
+﻿namespace pkgchk
+
+open System
+open System.Diagnostics
+open FSharp.Data
+
+type ScaData = JsonProvider<"ScaSample.json">
+
+type ScaHit =
+    { framework: string
+      projectPath: string
+      packageId: string
+      resolvedVersion: string
+      severity: string
+      advisoryUri: string }
+
+module Sca =
+
+    let createProcess path includeTransitive =
+        let p = new Process()
+
+        p.StartInfo.UseShellExecute <- false
+        p.StartInfo.RedirectStandardOutput <- true
+        p.StartInfo.FileName <- "dotnet"
+        p.StartInfo.CreateNoWindow <- true
+        p.StartInfo.WindowStyle <- ProcessWindowStyle.Hidden
+        p.StartInfo.RedirectStandardError <- true
+        p.StartInfo.RedirectStandardOutput <- true
+
+        let transitives =
+            match includeTransitive with
+            | true -> "--include-transitive"
+            | _ -> ""
+
+        let args =
+            if String.IsNullOrWhiteSpace path then
+                sprintf " list package --vulnerable %s --format json --output-version 1 " transitives
+            else
+                sprintf " list %s package --vulnerable %s --format json --output-version 1 " path transitives
+
+        p.StartInfo.Arguments <- args
+
+        p
+
+
+    let get (proc: Process) =
+        try
+            if proc.Start() then
+                let out = proc.StandardOutput.ReadToEnd()
+                let err = proc.StandardError.ReadToEnd()
+                proc.WaitForExit()
+
+                if (String.IsNullOrWhiteSpace(err)) then
+                    Choice1Of2(out)
+                else
+                    Choice2Of2(err)
+            else
+                Choice2Of2("Cannot start process")
+        with ex ->
+            Choice2Of2(ex.Message)
+
+    let parse json =
+        
+        try
+            let r = ScaData.Parse(json)
+
+            let topLevelVuls =
+                r.Projects
+                |> Seq.collect (fun p ->
+                    p.Frameworks
+                    |> Seq.collect (fun f ->
+                        f.TopLevelPackages
+                        |> Seq.collect (fun tp ->
+                            tp.Vulnerabilities
+                            |> Seq.map (fun v ->
+                                { ScaHit.projectPath = p.Path
+                                  framework = f.Framework
+                                  packageId = tp.Id
+                                  resolvedVersion = tp.ResolvedVersion
+                                  severity = v.Severity
+                                  advisoryUri = v.Advisoryurl }))))
+
+            let transitiveVuls =
+                r.Projects
+                |> Seq.collect (fun p ->
+                    p.Frameworks
+                    |> Seq.collect (fun f ->
+                        f.TransitivePackages
+                        |> Seq.collect (fun tp ->
+                            tp.Vulnerabilities
+                            |> Seq.map (fun v ->
+                                { ScaHit.projectPath = p.Path
+                                  framework = f.Framework
+                                  packageId = tp.Id
+                                  resolvedVersion = tp.ResolvedVersion
+                                  severity = v.Severity
+                                  advisoryUri = v.Advisoryurl }))))
+
+            let hits = topLevelVuls |> Seq.append transitiveVuls |> List.ofSeq
+            Choice1Of2 hits 
+        with
+        | ex -> Choice2Of2 ("An error occurred parsing results" + Environment.NewLine + ex.Message)
+
+    let formatHits (hits: seq<ScaHit>) =
+        let fmt (hit: ScaHit) =
+            seq {
+                sprintf "Severity:         %s" hit.severity
+                sprintf "Package:          %s" hit.packageId
+                sprintf "Resolved version: %s" hit.resolvedVersion
+                sprintf "Advisory URL:     %s" hit.advisoryUri
+                sprintf "Project:          %s" hit.projectPath
+            }
+
+        let lines = hits |> Seq.collect fmt
+        String.Join(Environment.NewLine, lines)
