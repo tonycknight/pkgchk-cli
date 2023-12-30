d459d03a614e583479f055afd3912246a8d68b20
tonycknight <tonycknight@users.noreply.github.com>
2023-12-30 21:56:25 +0000

    first sca module tests

--- tests/pkgchk-cli.tests/Program.fs
+++ tests/pkgchk-cli.tests/Program.fs
@@ -1,3 +1,5 @@
+namespace pkgchk.tests
+
 module Program =
     [<EntryPoint>]
     let main _ = 0
---
+++ tests/pkgchk-cli.tests/ScaTests.fs
@@ -0,0 +1,48 @@
+﻿namespace pkgchk.tests
+
+open FsUnit.Xunit
+open Xunit
+
+module ScaTests=
+
+    let getFile filename =
+        let assembly = System.Reflection.Assembly.GetExecutingAssembly()
+        let nspace = assembly.GetExportedTypes().[0].Namespace
+        assembly.GetManifestResourceStream($"{nspace}.{filename}")
+        
+    [<Theory>]
+    [<InlineData(" ")>]
+    [<InlineData("ABC")>]
+    let ``parse of plain text``(text)=        
+        match text |> pkgchk.Sca.parse with
+        | Choice2Of2 msg -> ignore 0
+        | _ -> failwith "No error raised"
+
+    [<Fact>]
+    let ``parse for vulnerabilities``()=
+        
+        use f = getFile "ScaSampleWithVulnerabilities.json"
+
+        use reader = new System.IO.StreamReader(f)
+
+        let r = reader.ReadToEnd() |> pkgchk.Sca.parse
+        
+        match r with
+        | Choice1Of2 xs -> 
+            match xs with
+            | [] -> failwith "Empty list returned"
+            | [ y; x ] -> 
+                x.framework |> should equal "net7.0"
+                x.packageId |> should equal "System.Net.Http"
+                x.resolvedVersion |> should equal "4.3.0"
+                x.severity |> should equal "Critical"
+                x.advisoryUri |> should not' (be NullOrEmptyString)
+
+                y.framework |> should equal "net7.0"
+                y.packageId |> should equal "System.Text.RegularExpressions"
+                y.resolvedVersion |> should equal "4.3.1"
+                y.severity |> should equal "High"                
+                y.advisoryUri |> should not' (be NullOrEmptyString)
+            | _ -> failwith "Unrecognised list returned"
+        | _ -> failwith "No error raised"
+
