f691dacfc7cce1d68cdef4c6aff0e94af917d013
tonycknight <tonycknight@users.noreply.github.com>
2024-01-01 15:43:38 +0000

    top-level deprecations

--- src/pkgchk-cli/Sca.fs
+++ src/pkgchk-cli/Sca.fs
@@ -16,7 +16,8 @@ type ScaHit =
       packageId: string
       resolvedVersion: string
       severity: string
-      advisoryUri: string }
+      advisoryUri: string
+      commentary: string}
 
 module Sca =
 
@@ -31,6 +32,16 @@ module Sca =
         else
             sprintf " list %s package --vulnerable %s --format json --output-version 1 " path transitives
 
+    let scanDeprecationsArgs includeTransitive path =
+        let transitives =
+            match includeTransitive with
+            | true -> "--include-transitive"
+            | _ -> ""
+
+        if String.IsNullOrWhiteSpace path then
+            sprintf " list package --deprecated %s --format json --output-version 1 " transitives
+        else
+            sprintf " list %s package --deprecated %s --format json --output-version 1 " path transitives
 
     let parse json =
 
@@ -52,7 +63,26 @@ module Sca =
                                   packageId = tp.Id
                                   resolvedVersion = tp.ResolvedVersion
                                   severity = v.Severity
+                                  commentary = ""
                                   advisoryUri = v.Advisoryurl }))))
+            let topLevelDeprecations =
+                r.Projects
+                |> Seq.collect (fun p ->
+                    p.Frameworks
+                    |> Seq.collect (fun f ->
+                        f.TopLevelPackages
+                        |> Seq.collect (fun tp ->
+                            tp.DeprecationReasons
+                            |> Seq.filter (String.IsNullOrWhiteSpace >> not)
+                            |> Seq.map (fun d ->
+                                { ScaHit.projectPath = System.IO.Path.GetFullPath(p.Path)
+                                  kind = ScaHitKind.Deprecated
+                                  framework = f.Framework
+                                  packageId = tp.Id
+                                  resolvedVersion = tp.ResolvedVersion
+                                  severity = ""
+                                  commentary = d // tp.DeprecationReasons |> String.join "; "
+                                  advisoryUri = "" }))))
 
             let transitiveVuls =
                 r.Projects
@@ -69,9 +99,10 @@ module Sca =
                                   packageId = tp.Id
                                   resolvedVersion = tp.ResolvedVersion
                                   severity = v.Severity
+                                  commentary = ""
                                   advisoryUri = v.Advisoryurl }))))
 
-            let hits = topLevelVuls |> Seq.append transitiveVuls |> List.ofSeq
+            let hits = topLevelVuls |> Seq.append transitiveVuls |> Seq.append topLevelDeprecations |> List.ofSeq
             Choice1Of2 hits
         with ex ->
             Choice2Of2("An error occurred parsing results" + Environment.NewLine + ex.Message)
