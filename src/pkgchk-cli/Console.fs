efe9a9cb5f54de6cc3fee7b8348b6de46f96a419
tonycknight <tonycknight@users.noreply.github.com>
2024-01-02 08:32:08 +0000

    fix reasons markup

--- src/pkgchk-cli/Console.fs
+++ src/pkgchk-cli/Console.fs
@@ -70,7 +70,7 @@ module Console =
                         (formatReasons hit.reasons)
                         hit.suggestedReplacement
                 else if (hit.reasons |> Array.isEmpty |> not) then
-                    sprintf "                    [italic]%s " (formatReasons hit.reasons)
+                    sprintf "                    [italic]%s[/]" (formatReasons hit.reasons)
 
                 ""
             }
