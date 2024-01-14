02d1306578936d884955c2a3d4ddc5beec059024
tonycknight <tonycknight@users.noreply.github.com>
2024-01-14 21:31:11 +0000

    joinPretty

--- src/pkgchk-cli/Utils.fs
+++ src/pkgchk-cli/Utils.fs
@@ -5,10 +5,27 @@ open System.Diagnostics
 
 module String =
     [<DebuggerStepThrough>]
-    let join (separator) (lines: seq<string>) = String.Join(separator, lines)
+    let join separator (lines: seq<string>) = String.Join(separator, lines)
 
     [<DebuggerStepThrough>]
     let joinLines (lines: seq<string>) = join Environment.NewLine lines
+
+    let joinPretty (separator: string) (values: string list) =
+        let rec concat (values: string list) (accum: System.Text.StringBuilder) =
+            let suffix (sep: string) (accum: System.Text.StringBuilder) =
+                if accum.Length > 0 then accum.Append(sep) else accum
+
+            match values with
+            | [] -> accum.ToString()
+            | [ x ] ->
+                let accum = accum |> suffix $" {separator} "
+
+                accum.Append(x).ToString()
+            | h :: t ->
+                let accum = accum |> suffix ", "
+                accum.Append(h) |> concat t
+
+        concat values (new System.Text.StringBuilder())
 
     [<DebuggerStepThrough>]
     let isEmpty = String.IsNullOrWhiteSpace
---
+++ tests/pkgchk-cli.tests/StringsTests.fs
@@ -0,0 +1,41 @@
+﻿namespace pkgchk.tests
+
+open System
+open FsUnit.Xunit
+open FsCheck
+open FsCheck.Xunit
+
+
+module StringsTests =
+
+    [<Property>]
+    let ``join conjugates strings`` (count: PositiveInt) =
+        let values = [ 0 .. count.Get ] |> Seq.map (fun _ -> "A") |> Array.ofSeq
+
+        let result = values |> pkgchk.String.join " "
+
+        let expected = result.Split(' ', StringSplitOptions.None)
+
+        expected = values
+
+    [<Property(Verbose = true)>]
+    let ``joinPretty`` (count: PositiveInt) =
+        let values = [ 1 .. count.Get ] |> Seq.map (fun _ -> "A") |> List.ofSeq
+        let separator = "or"
+
+        let result = values |> pkgchk.String.joinPretty separator
+
+        let decomp =
+            result.Split(' ', StringSplitOptions.None)
+            |> Array.map (fun s -> s.Replace(",", ""))
+            |> Array.rev
+
+        let counts = decomp |> Array.countBy id |> Map.ofSeq
+
+        match count.Get with
+        | 1 -> counts.["A"] = count.Get && decomp |> Array.contains separator |> not
+
+        | x ->
+            let pos = Array.IndexOf(decomp, separator)
+
+            counts.[separator] = 1 && counts.["A"] = count.Get && pos = 1
