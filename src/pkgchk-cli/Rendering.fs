bd2f9f928ee603cf0ae3245709feff787ca052b0
tonycknight <tonycknight@users.noreply.github.com>
2024-01-20 14:45:00 +0000

    shrink transitive label

--- src/pkgchk-cli/Rendering.fs
+++ src/pkgchk-cli/Rendering.fs
@@ -7,7 +7,7 @@ module Rendering =
 
     let formatHitKind =
         function
-        | ScaHitKind.VulnerabilityTransitive -> "Vulnerable transitive package"
+        | ScaHitKind.VulnerabilityTransitive -> "Vulnerable transitive"
         | ScaHitKind.Vulnerability -> "Vulnerable package"
         | ScaHitKind.Deprecated -> "Deprecated package"
 
