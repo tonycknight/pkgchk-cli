3ff03cd8c8928d7dce5f78e53067af99b9517153
tonycknight <tonycknight@users.noreply.github.com>
2024-01-21 16:28:19 +0000

    more tests

---
+++ tests/pkgchk-cli.tests/ScaHitTests.fs
@@ -0,0 +1,22 @@
+﻿namespace pkgchk.tests
+
+open System
+open FsUnit.Xunit
+open Xunit
+
+module ScaHitTests =
+
+    [<Fact>]
+    let ``ScaHit.empty returns empty`` () =
+        let result = pkgchk.ScaHit.empty
+
+        result.projectPath |> should be EmptyString
+        result.reasons |> should haveLength 0
+        result.severity |> should be EmptyString
+        result.advisoryUri |> should be EmptyString
+        result.kind |> should equal pkgchk.ScaHitKind.Vulnerability
+        result.packageId |> should be EmptyString
+        result.resolvedVersion |> should be EmptyString
+        result.alternativePackageId |> should be EmptyString
+        result.suggestedReplacement |> should be EmptyString
+        result.framework |> should be EmptyString
