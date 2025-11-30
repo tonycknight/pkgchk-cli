1be6aa34ead11e207e4f38335d5449a23be3f362
Tony Knight <tonycknight@users.noreply.github.com>
2025-11-30 15:16:31 +0000

    post upgrade report to github

--- src/pkgchk-cli/PackageUpgradeCommand.fs
+++ src/pkgchk-cli/PackageUpgradeCommand.fs
@@ -14,9 +14,45 @@ type PackageUpgradeCommandSettings() =
     [<DefaultValue("")>]
     member val OutputDirectory = "" with get, set
 
+    [<CommandOption("--github-token", IsHidden = true)>]
+    [<Description("A Github token.")>]
+    [<DefaultValue("")>]
+    member val GithubToken = "" with get, set
+
+    [<CommandOption("--github-repo", IsHidden = true)>]
+    [<Description("The name of the Github repository in the form <owner>/<repo>, e.g. github/octokit.")>]
+    [<DefaultValue("")>]
+    member val GithubRepo = "" with get, set
+
+    [<CommandOption("--github-title", IsHidden = true)>]
+    [<Description("The Github report title.")>]
+    [<DefaultValue("")>]
+    member val GithubSummaryTitle = "" with get, set
+
+    [<CommandOption("--github-pr", IsHidden = true)>]
+    [<Description("Pull request ID.")>]
+    [<DefaultValue("")>]
+    member val GithubPrId = "" with get, set
+
+    [<CommandOption("--github-commit", IsHidden = true)>]
+    [<Description("Commit hash.")>]
+    [<DefaultValue("")>]
+    member val GithubCommit = "" with get, set
+        
 [<ExcludeFromCodeCoverage>]
 type PackageUpgradeCommand(nuget: Tk.Nuget.INugetClient) =
     inherit Command<PackageUpgradeCommandSettings>()
+
+    let genComment trace (settings: PackageUpgradeCommandSettings, hits) attempt =        
+        let markdown =
+            hits
+            |> Markdown.generateUpgrades
+            |> String.joinLines
+            
+        if markdown.Length < Github.maxCommentSize then
+            GithubComment.create settings.GithubSummaryTitle markdown
+        else
+            GithubComment.create settings.GithubSummaryTitle "_The report is too big for Github - Please check logs_"            
 
     override _.Execute(context, settings) =
         let trace = Commands.trace settings.TraceLogging
@@ -50,7 +86,7 @@ type PackageUpgradeCommand(nuget: Tk.Nuget.INugetClient) =
                     }
 
                 Commands.renderTables renderables
-                                
+                    
                 if settings.OutputDirectory <> "" then
                     trace "Building reports..."
                     let reportFile =
@@ -64,5 +100,29 @@ type PackageUpgradeCommand(nuget: Tk.Nuget.INugetClient) =
                     $"{Environment.NewLine}Report file [link={reportFile}]{reportFile}[/] built."
                     |> Console.italic
                     |> Commands.console
+
+                if
+                    String.isNotEmpty settings.GithubToken
+                    && String.isNotEmpty settings.GithubRepo
+                    && (String.isNotEmpty settings.GithubPrId || String.isNotEmpty settings.GithubCommit)
+                then
+                    trace "Building Github reports..."
+                    let prId = String.toInt settings.GithubPrId
+                    let repo = Github.repo settings.GithubRepo
+                    let client = Github.client settings.GithubToken
+                    let commit = settings.GithubCommit
+
+                    let comment = genComment trace (settings, hits) 0
+
+                    if String.isNotEmpty settings.GithubPrId then
+                        trace $"Posting {comment.title} PR comment to Github repo {repo}..."
+                        let _ = (comment |> Github.setPrComment trace client repo prId).Result
+                        $"{comment.title} report sent to Github." |> Console.italic |> Commands.console
+
+                    if String.isNotEmpty settings.GithubCommit then
+                        trace $"Posting {comment.title} build check to Github repo {repo}..."
+                        
+                        (comment |> Github.createCheck trace client repo commit true).Result
+                        |> ignore
 
                 ReturnCodes.validationOk
