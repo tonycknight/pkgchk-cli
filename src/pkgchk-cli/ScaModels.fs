2b98e0865a039c1221f982ea33038ed0b83f25e5
Tony Knight <tonycknight@users.noreply.github.com>
2025-12-22 01:06:35 +0000

    move out sca models

---
+++ src/pkgchk-cli/ScaModels.fs
@@ -0,0 +1,37 @@
+﻿namespace pkgchk
+
+type ScaHitKind =
+    | Vulnerability
+    | VulnerabilityTransitive
+    | Deprecated
+    | Dependency
+    | DependencyTransitive
+
+type ScaHit =
+    { kind: ScaHitKind
+      framework: string
+      projectPath: string
+      packageId: string
+      resolvedVersion: string
+      severity: string
+      advisoryUri: string
+      reasons: string[]
+      suggestedReplacement: string
+      alternativePackageId: string }
+
+    static member empty =
+        { ScaHit.projectPath = ""
+          framework = ""
+          packageId = ""
+          resolvedVersion = ""
+          severity = ""
+          reasons = [||]
+          suggestedReplacement = ""
+          alternativePackageId = ""
+          advisoryUri = ""
+          kind = ScaHitKind.Vulnerability }
+
+type ScaHitSummary =
+    { kind: ScaHitKind
+      severity: string
+      count: int }
