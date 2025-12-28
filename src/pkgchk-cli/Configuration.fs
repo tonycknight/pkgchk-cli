72db27bcf9d1ef6f0e2fc1261aaeb4844e29ce98
Tony Knight <tonycknight@users.noreply.github.com>
2025-12-28 02:39:53 +0000

    clean

--- src/pkgchk-cli/Configuration.fs
+++ src/pkgchk-cli/Configuration.fs
@@ -1,7 +1,6 @@
 ﻿namespace pkgchk
 
 open System
-open pkgchk.Combinators
 open YamlDotNet.Serialization
 
 [<CLIMutable>]
@@ -28,25 +27,3 @@ module Config =
             includedPackages = r.includedPackages |> Option.nullDefault [||]
             excludedPackages = r.excludedPackages |> Option.nullDefault [||]
             severities = r.severities |> Option.nullDefault [||] }
-
-    // TODO: defunct...
-    let filterPackages (config: ScanConfiguration) (hits: pkgchk.ScaHit list) =
-        let inclusionMap =
-            config.includedPackages
-            |> HashSet.ofSeq System.StringComparer.InvariantCultureIgnoreCase
-
-        let exclusionMap =
-            config.excludedPackages
-            |> HashSet.ofSeq System.StringComparer.InvariantCultureIgnoreCase
-
-        let included (hit: ScaHit) =
-            match inclusionMap.Count with
-            | 0 -> true
-            | x -> inclusionMap.Contains hit.packageId
-
-        let excluded (hit: ScaHit) =
-            match exclusionMap.Count with
-            | 0 -> true
-            | x -> exclusionMap.Contains hit.packageId |> not
-
-        hits |> List.filter (included &&>> excluded)
