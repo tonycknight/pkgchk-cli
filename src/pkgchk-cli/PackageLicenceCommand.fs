11428c88ebff8b1f96bd7013aee84c9637c06ff1
Tony Knight <tonycknight@users.noreply.github.com>
2026-04-24 20:10:37 +0100

    cleanup

--- src/pkgchk-cli/PackageLicenceCommand.fs
+++ src/pkgchk-cli/PackageLicenceCommand.fs
@@ -47,7 +47,6 @@ type PackageLicenceCommand(nuget: INugetClient) =
     let filterLicenceHits (context: ApplicationContext) (results: ApplicationScanResults) =
 
         let isHit (hit: ScaHit) =
-            // TODO: what if no allowed/disallowed licence names provided?
             match (Context.isDisllowedLicence context.options hit, Context.isAllowedLicence context.options hit) with
             | (None, None) -> not context.options.ignoreMissingLicence
             | (Some false, None) -> false
