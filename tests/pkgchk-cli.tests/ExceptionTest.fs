6e2abb7b09a470a6365bf7134dfb4be1d0601050
Tony Knight <tonycknight@users.noreply.github.com>
2026-06-21 14:17:09 +0100

    exception handling

---
+++ tests/pkgchk-cli.tests/ExceptionTest.fs
@@ -0,0 +1,22 @@
+namespace pkgchk.tests
+
+open FsCheck.Xunit
+
+module ExceptionTest =
+    
+    [<Property>]
+    let ``iter when exception raised, the handler is triggered`` (value: string) =
+        let mutable triggerd = false
+        let f = invalidOp
+
+        value |> pkgchk.Exception.iter invalidOp (fun x -> triggerd <- true)
+        
+        triggerd
+
+    [<Property>]
+    let ``iter when no exception raised, the handler is not triggered`` (value: string) =
+        let mutable triggerd = false
+        
+        value |> pkgchk.Exception.iter ignore (fun x -> triggerd <- true)
+        
+        not triggerd
